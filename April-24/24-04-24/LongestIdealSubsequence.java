e45c01d1033bf8e30aacff52be0100f00e2db9d6
NeuYash <pawar.ya@northeastern.edu>
2024-04-25 13:04:59 -0400

    24th April

---
+++ April-24/24-04-24/LongestIdealSubsequence.java
@@ -0,0 +1,17 @@
+package com.leetcode.April;
+//Problem Link:https://leetcode.com/problems/longest-ideal-subsequence
+//O(nk) O(150)
+public class LongestIdealSubsequence {
+	class Solution {
+	     public int longestIdealString(String s, int k) {
+	        int res = 0, n = s.length(), dp[] = new int[150];
+	        for (int ci = 0; ci < n; ++ci) {
+	            int i = s.charAt(ci);
+	            for (int j = i - k; j <= i + k; ++j)
+	                dp[i] = Math.max(dp[i], dp[j]);
+	            res = Math.max(res, ++dp[i]);
+	        }
+	        return res;
+	    }
+	}
+}
